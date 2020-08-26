87497b283624a6c473c3905ceac8d4e55de058f2
Mat Grube <m.grube@outlook.com>
2020-08-26 20:09:55 +0200

    Cleanup

--- script.js
+++ script.js
@@ -1,32 +1,10 @@
 const content = document.getElementById('main_section');
-console.log(content);
-content.style.backgroundColor = "#581d22";
-
-// BUTTONS DECLARATIONS //
 
 const buttonAbout = document.getElementById("button_about");
-const buttons = document.querySelectorAll(".nav_button");
+const buttonPortfolio = document.getElementById("button_portfolio");
 const buttonContact = document.getElementById("button_contact");
+const buttonTechnologies = document.getElementById("button_technologies");
 
-const buildComponent = (parentComponent, componentMarkup, componentClass) => {
-    const elementName = document.createElement(componentMarkup);
-    elementName.classList.add(componentClass);
-    parentComponent.appendChild(elementName);
-    return elementName;
-}
-
-
-const about = () => {
-    content.innerHTML = `
-        <H1 class="about_name"> Mateusz Grube </H1>
-    `;
-};
-
-const contact = () => {
-    content.innerHTML = `
-        <H2 class="contact_title">Contact me!"</H2>
-    `;
-};
 
 buttonAbout.addEventListener("click", about);
 buttonContact.addEventListener("click", contact);
--- src/about.js
+++
@@ -1,8 +0,0 @@
-const About = () => {
-    content.innerHTML = `
-        <p>Tutaj jest jakiś tekst spradzający działanie ABOUT z about.js</p>
-    `;
-    console.log("About dziala");
-};
-
-export default About;
