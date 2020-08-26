70f1ad136bf9aac4f6fdd87f451e5032ecc19a5e
Mat Grube <m.grube@outlook.com>
2020-08-26 20:09:10 +0200

    Rebuilt structure | Made elements placement

---
+++ src/about.js
@@ -0,0 +1,8 @@
+const About = () => {
+    content.innerHTML = `
+        <p>Tutaj jest jakiś tekst spradzający działanie ABOUT z about.js</p>
+    `;
+    console.log("About dziala");
+};
+
+export default About;
