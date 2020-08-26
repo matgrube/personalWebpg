a1d1fbe2aad5e2863e590b1e22ca39183d4352a9
Mat Grube <m.grube@outlook.com>
2020-08-27 01:06:38 +0200

    Pre-styled buttons (for further styling | added basic onclick mechanism for about arrow

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const content = document.getElementById('main_section');
-
 const buttonAbout = document.getElementById("button_about");
 const buttonPortfolio = document.getElementById("button_portfolio");
 const buttonContact = document.getElementById("button_contact");
 const buttonTechnologies = document.getElementById("button_technologies");
 
+const aboutPage = document.querySelector('.about');
+const navPage = document.querySelector('.arrow-navigation');
 
-buttonAbout.addEventListener("click", about);
-buttonContact.addEventListener("click", contact);
+
+buttonAbout.addEventListener("click", () => {
+    aboutPage.classList.add('visible');
+    navPage.classList.add('invisible');
+    console.log(aboutPage);
+    console.log(navPage);
+});
